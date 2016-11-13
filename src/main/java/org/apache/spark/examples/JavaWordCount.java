2646d1665f7721d01389e6f87ce334852ac11e82
volodymyr shpynta <vladimir.shpinta@gmail.com>
2016-11-14 00:06:19 +0200

    Adjusted Word Count example: refactored to use lambdas

--- src/main/java/org/apache/spark/examples/JavaWordCount.java
+++ src/main/java/org/apache/spark/examples/JavaWordCount.java
@@ -18,12 +18,8 @@
 package org.apache.spark.examples;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -40,32 +36,13 @@ public final class JavaWordCount {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
         JavaRDD<String> lines = ctx.textFile(fileName, 1);
-
-        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterable<String> call(String s) {
-                return Arrays.asList(SPACE.split(s));
-            }
-        });
-
-        JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
-            public Tuple2<String, Integer> call(String s) {
-                return new Tuple2<String, Integer>(s, 1);
-            }
-        });
-
-        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer i1, Integer i2) {
-                return i1 + i2;
-            }
-        });
-
-        List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?, ?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
-        }
+        List<Tuple2<String, Integer>> countedWords = lines
+                .flatMap(line -> Arrays.asList(SPACE.split(line)))
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((i1, i2) -> i1 + i2) //count the same words (which are the keys)
+                .collect();
+        countedWords.forEach(wordCountTuple ->
+                System.out.println(wordCountTuple._1() + ": " + wordCountTuple._2()));
         ctx.stop();
     }
 }
