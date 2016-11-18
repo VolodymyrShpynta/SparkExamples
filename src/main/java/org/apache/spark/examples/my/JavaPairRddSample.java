2a313b57ea3817af3f60e0b80fc2a51775e17efd
VolodymyrShpynta <vshpynta@cogniance.com>
2016-11-18 18:25:00 +0200

    Added JavaPairRddSample

---
+++ src/main/java/org/apache/spark/examples/my/JavaPairRddSample.java
@@ -0,0 +1,37 @@
+package org.apache.spark.examples.my;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by vshpynta on 18.11.16.
+ */
+public class JavaPairRddSample {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaPairRddSample");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        JavaPairRDD<Integer, Integer> pairRDD = ctx.parallelizePairs(Arrays.asList(
+                new Tuple2<>(1, 2),
+                new Tuple2<>(3, 4),
+                new Tuple2<>(3, 6)));
+
+        System.out.println(pairRDD
+                        .flatMapValues(v -> listFromTo(v, 5))
+                        .collect()
+        );
+    }
+
+    private static List<Integer> listFromTo(final int from, final int to) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = from; i <= to; i++) {
+            list.add(i);
+        }
+        return list;
+    }
+}
