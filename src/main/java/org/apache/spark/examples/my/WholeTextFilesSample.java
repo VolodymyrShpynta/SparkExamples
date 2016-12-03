9823e86b164009fa7a1cb674cb2aa00446d5849e
Volodymyr <vladimir.shpinta@gmail.com>
2016-12-04 01:02:35 +0200

    Added WholeTextFilesSample

---
+++ src/main/java/org/apache/spark/examples/my/WholeTextFilesSample.java
@@ -0,0 +1,30 @@
+package org.apache.spark.examples.my;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by volodymyr on 03.12.16.
+ */
+public class WholeTextFilesSample {
+    public static void main(String[] args) throws Exception {
+//        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
+        String filesPath = "src/main/resources/bucket/**/*";
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        JavaPairRDD<String, String> pairRDD = ctx.wholeTextFiles(filesPath)
+                .mapValues(WholeTextFilesSample::mapV);
+        System.out.println(pairRDD.collect());
+        ctx.stop();
+    }
+
+    private static String mapV(String v) {
+        List<String> strings = Arrays.asList(v.split("\n"));
+        System.out.println("Value: " + strings);
+        return v;
+    }
+}
