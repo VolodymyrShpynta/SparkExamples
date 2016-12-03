0c7152e909fef419ddfd4711766944d896b238aa
Volodymyr <vladimir.shpinta@gmail.com>
2016-12-04 01:33:53 +0200

    Adjusted WholeTextFilesSample

--- src/main/java/org/apache/spark/examples/my/WholeTextFilesSample.java
+++ src/main/java/org/apache/spark/examples/my/WholeTextFilesSample.java
@@ -3,6 +3,7 @@ package org.apache.spark.examples.my;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,9 +18,15 @@ public class WholeTextFilesSample {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
         JavaPairRDD<String, String> pairRDD = ctx.wholeTextFiles(filesPath)
+                .mapToPair(tuple -> new Tuple2<>(substring(tuple._1()), tuple._2()))
                 .mapValues(WholeTextFilesSample::mapV);
         System.out.println(pairRDD.collect());
         ctx.stop();
+    }
+
+    private static String substring(String str) {
+        String searchWord = "bucket";
+        return str.substring(str.indexOf(searchWord) + searchWord.length());
     }
 
     private static String mapV(String v) {
