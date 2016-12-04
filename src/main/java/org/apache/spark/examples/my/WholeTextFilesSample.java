79d6bae9b3a30ba6325ea028df58e0ef14424da2
Volodymyr <vladimir.shpinta@gmail.com>
2016-12-04 14:22:03 +0200

    Adjusted WholeTextFilesSample

--- src/main/java/org/apache/spark/examples/my/WholeTextFilesSample.java
+++ src/main/java/org/apache/spark/examples/my/WholeTextFilesSample.java
@@ -1,7 +1,10 @@
 package org.apache.spark.examples.my;
 
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
@@ -15,23 +18,42 @@ public class WholeTextFilesSample {
     public static void main(String[] args) throws Exception {
 //        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
         String filesPath = "src/main/resources/bucket/**/*";
-        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("WholeTextFilesSample");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        JavaPairRDD<String, String> pairRDD = ctx.wholeTextFiles(filesPath)
-                .mapToPair(tuple -> new Tuple2<>(substring(tuple._1()), tuple._2()))
-                .mapValues(WholeTextFilesSample::mapV);
-        System.out.println(pairRDD.collect());
+        ctx.wholeTextFiles(filesPath)
+                .mapToPair(WholeTextFilesSample::keysRelativePath)
+                .flatMapValues(WholeTextFilesSample::splitValues)
+                .mapToPair(WholeTextFilesSample::convertToWritable)
+                .saveAsHadoopFile("target/out", Text.class, Text.class, RddMultipleTextOutputFormat.class);
         ctx.stop();
     }
 
+    private static Tuple2<String, String> keysRelativePath(Tuple2<String, String> tuple) {
+        return new Tuple2<>(substring(tuple._1()), tuple._2());
+    }
+
+    private static Tuple2<WritableComparable, WritableComparable> convertToWritable(Tuple2<String, String> tuple) {
+        return new Tuple2<>(new Text(tuple._1()), new Text(tuple._2()));
+    }
+
     private static String substring(String str) {
-        String searchWord = "bucket";
+        String searchWord = "bucket/";
         return str.substring(str.indexOf(searchWord) + searchWord.length());
     }
 
-    private static String mapV(String v) {
-        List<String> strings = Arrays.asList(v.split("\n"));
-        System.out.println("Value: " + strings);
-        return v;
+    private static List<String> splitValues(String v) {
+        return Arrays.asList(v.split("\n"));
+    }
+
+    private static class RddMultipleTextOutputFormat extends MultipleTextOutputFormat{
+        @Override
+        protected Object generateActualKey(Object key, Object value) {
+            return NullWritable.get();
+        }
+
+        @Override
+        protected String generateFileNameForKeyValue(Object key, Object value, String name) {
+            return key.toString();
+        }
     }
 }
