aa10b8ea8d02ff8e1679c2e018b89af196a22ab8
VolodymyrShpynta <vshpynta@cogniance.com>
2017-01-25 17:23:25 +0200

    Adjusted YahooFinanceMetrics

--- src/main/java/org/apache/spark/examples/my/YahooFinanceMetrics.java
+++ src/main/java/org/apache/spark/examples/my/YahooFinanceMetrics.java
@@ -6,13 +6,17 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.util.Assert;
 import scala.Tuple2;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,32 +28,67 @@ public final class YahooFinanceMetrics {
     private static final String URL_PATTERN = "http://real-chart.finance.yahoo.com/table.csv?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s&g=d&ignore=.csv";
     private static final String PRICES_FILE_LOCATION = "target/yahoo-finance-metrics.csv";
     private static final String CSV_COLUMN_SEPARATOR = ",";
-    public static final int PRICE_COLUMN_NUMBER = 6;
+    private static final int PRICE_COLUMN_NUMBER = 6;
+    private static final int DATE_COLUMN_NUMBER = 0;
 
 
     public static void main(String[] args) throws Exception {
-//        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
+//        SparkConf sparkConf = new SparkConf().setAppName("YahooFinanceMetrics");
         downloadPrices("GOOG");
-        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("YahooFinanceMetrics");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        JavaRDD<String> lines = ctx.textFile(PRICES_FILE_LOCATION, 1);
-        final JavaPairRDD<String, Long> enumeratedLines = lines.zipWithIndex()
-                .filter(tuple2 -> tuple2._2() > 0)  //skip csv header
+        JavaRDD<String> lines = ctx.textFile(PRICES_FILE_LOCATION, 1)
+                .mapPartitionsWithIndex(YahooFinanceMetrics::removeCsvHeader, false)  //skip csv header
                 .cache();
-        final List<Double> dailyPrices = enumeratedLines
-                .mapPartitions(tuple2Iterator -> streamOf(tuple2Iterator)
-                        .map(Tuple2::_1) //Get line
-                        .map(line -> line.split(CSV_COLUMN_SEPARATOR)[PRICE_COLUMN_NUMBER].trim())  //Get price column value
+
+        final List<Double> dailyPrices = lines
+                .mapPartitions(linesIterator -> streamOf(linesIterator)
+                        .map(YahooFinanceMetrics::getPrice)
                         .map(Double::valueOf)
                         .collect(Collectors.toList()))
                 .collect();
 
-        System.out.println(enumeratedLines.collect());
+        final JavaPairRDD<String, String> sortedLines = lines.mapPartitionsToPair(linesIterator -> streamOf(linesIterator)
+                        .map(line -> new Tuple2<>(getDate(line), line))
+                        .collect(Collectors.toList())
+        )
+                .sortByKey(new DateComparator(), false);
+
+//        final JavaRDD<Tuple2<Long, String>> sortedLines = enumeratedLines.mapPartitions(tuple2Iterator -> streamOf(tuple2Iterator)
+//                .map(YahooFinanceMetrics::reverseTuple) //Reverse to (Long,String) tuple
+//                .collect(Collectors.toList()))
+//                .cache();
+
+        System.out.println(lines.collect());
         System.out.println(dailyPrices);
+        System.out.println(sortedLines.collect());
 
 
         ctx.stop();
 
+    }
+
+    private static String getPrice(final String line) {
+        return line.split(CSV_COLUMN_SEPARATOR)[PRICE_COLUMN_NUMBER].trim(); //Get price column value
+    }
+
+    private static String getDate(final String line) {
+        return line.split(CSV_COLUMN_SEPARATOR)[DATE_COLUMN_NUMBER].trim(); //Get date column value
+    }
+
+    // Assumed there is just one header line, in the first record
+    private static Iterator<String> removeCsvHeader(Integer partitionIndex, Iterator<String> partitionIterator) {
+        return partitionIndex == 0 ? removeFirstLine(partitionIterator) : partitionIterator;
+    }
+
+    private static Iterator<String> removeFirstLine(Iterator<String> partitionIterator) {
+        Assert.isTrue(partitionIterator.hasNext(), "There should be at least one record in the csv file");
+        partitionIterator.next();  //Just skip first line
+        return partitionIterator;
+    }
+
+    private static <K, V> Tuple2<V, K> reverseTuple(Tuple2<K, V> tuple) {
+        return new Tuple2<>(tuple._2(), tuple._1());
     }
 
     private static void downloadPrices(final String ticker) {
@@ -66,5 +105,12 @@ public final class YahooFinanceMetrics {
 
         return new URL(format(URL_PATTERN,
                 ticker, lastYear.getMonthValue(), lastYear.getDayOfMonth(), lastYear.getYear(), businessDate.getMonthValue(), businessDate.getDayOfMonth(), businessDate.getYear()));
+    }
+
+    private static class DateComparator implements Comparator<String>, Serializable {
+        @Override
+        public int compare(final String dateStr1, final String dateStr2) {
+            return LocalDate.parse(dateStr1).compareTo(LocalDate.parse(dateStr2));
+        }
     }
 }
